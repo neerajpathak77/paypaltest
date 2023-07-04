4d9121f99fbc418e7111e5d038cb3d970224a591
neeraj pathak <pathakn@mediamarktsaturn.com>
2023-07-04 10:13:00 +0200

    react cpmponent

--- paypal-client/src/App.js
+++ paypal-client/src/App.js
@@ -1,6 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { useEffect, useState } from 'react'
+
+const PayPalSdkExample = () => {
+  return (
+      <PayPalScriptProvider options={{ clientId: "AYPzEXwX0emtTccFnnx_xQv1tQ0PZ3eLWYd174D-OZ0w97DzvtXZ0PTwmD01XXMA0bF5SWoJBnViIVEd" }}>
+          <PayPalButtons
+              createOrder={(data, actions) => {
+                  // Here call the node js endpoint
+                  return actions.order.create({
+                      purchase_units: [
+                          {
+                              amount: {
+                                  value: "1.99",
+                              },
+                          },
+                      ],
+                  });
+              }}
+              onApprove={(data, actions) => {
+                // Here call the node js endpoint. This is actual finilization
+                  return actions.order.capture().then((details) => {
+                      const name = details.payer.name.given_name;
+                      alert(`Transaction completed by ${name}`);
+                  });
+              }}
+          />
+      </PayPalScriptProvider>
+  );
+}
 
 function App() {
 
@@ -17,6 +46,7 @@ function App() {
 
   return (
     <div className="App">
+      <PayPalSdkExample/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
